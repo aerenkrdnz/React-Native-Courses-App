87e32683e032633ee031112c57f431b581d3d8f4
Ali Eren Karadeniz <aerenkrdnz@gmail.com>
2024-08-24 21:50:17 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+    <StatusBar style="light" />
+
+    <View style={styles.container}>
+      
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
